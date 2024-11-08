e80a6d73c487c4f240ec9d14e389f898b8cfd1cc
Kyle McParland <kylemcparland@gmail.com>
2024-11-08 11:39:26 -0500

    Implement benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,7 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+  Time.now - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
